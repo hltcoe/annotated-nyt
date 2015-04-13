9b33795069cbff7f7731e15f7e57bc69eb1102b6
Max Thomas <max.thomas@jhu.edu>
2015-04-13 18:51:30 -0400

    Work in some utility functionality - still need to tackle body as list, etc.

---
+++ src/main/java/edu/jhu/hlt/annotatednyt/AnnotatedNYTDocument.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
+ * See LICENSE in the project root directory.
+ */
+
+package edu.jhu.hlt.annotatednyt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+import com.nytlabs.corpus.NYTCorpusDocument;
+
+/**
+ * Wrapper for the {@link NYTCorpusDocument} object, providing some
+ * extra utility.
+ */
+public class AnnotatedNYTDocument {
+
+  private final NYTCorpusDocument nytdoc;
+  
+  public AnnotatedNYTDocument(final NYTCorpusDocument nytdoc) {
+    this.nytdoc = nytdoc;
+  }
+  
+  public Integer getGuid() {
+    return this.nytdoc.getGuid();
+  }
+  
+  public List<String> getOnlineSectionAsList() {
+    List<String> onlineSectionList = new ArrayList<String>();
+    Optional<String> online = Optional.ofNullable(this.nytdoc.getSection());
+    online.ifPresent(str -> {
+      String[] split = str.split(";");
+      for (String s : split)
+        onlineSectionList.add(s);
+    });
+    
+    return onlineSectionList;
+  }
+  
+  public List<String> getLeadParagraphAsList() {
+    List<String> strList = new ArrayList<String>();
+    // TODO
+    return strList;
+  }
+  
+  public List<String> getBodyAsList() {
+    List<String> strList = new ArrayList<String>();
+    // TODO
+    return strList;    
+  }
+}
---
+++ src/main/java/edu/jhu/hlt/annotatednyt/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
+ * See LICENSE in the project root directory.
+ */
+
+/**
+ * Contains wrappers and utilities for the {@link com.nytlabs.corpus.NYTCorpusDocument} class.
+ */
+package edu.jhu.hlt.annotatednyt;
