0211e431ac14adbaa75c6f758776450c7db06811
Max Thomas <max.thomas@jhu.edu>
2015-04-13 18:52:00 -0400

    Fix up some headers.

--- src/main/java/edu/jhu/hlt/annotatednyt/AnnotatedNYTDocument.java
+++ src/main/java/edu/jhu/hlt/annotatednyt/AnnotatedNYTDocument.java
@@ -1,14 +1,12 @@
 /*
- * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
+ * Copyright 2012-2015 Johns Hopkins University HLTCOE. All rights reserved.
  * See LICENSE in the project root directory.
  */
-
 package edu.jhu.hlt.annotatednyt;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 
 import com.nytlabs.corpus.NYTCorpusDocument;
 
