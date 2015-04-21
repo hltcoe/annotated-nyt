cc0ac0a4574dff142b6ce3322a24eb35cd3d2495
max thomas <max.thomas@jhu.edu>
2015-04-21 17:12:42 -0400

    Update ANYTdocument API to better capture possibly null fields.

--- src/main/java/edu/jhu/hlt/annotatednyt/AnnotatedNYTDocument.java
+++ src/main/java/edu/jhu/hlt/annotatednyt/AnnotatedNYTDocument.java
@@ -79,36 +79,36 @@ public class AnnotatedNYTDocument {
     return this.nytdoc.getHeadline();
   }
 
-  public String getOnlineHeadline() {
-    return this.nytdoc.getOnlineHeadline();
+  public Optional<String> getOnlineHeadline() {
+    return Optional.ofNullable(this.nytdoc.getOnlineHeadline());
   }
 
-  public String getByline() {
-    return this.nytdoc.getByline();
+  public Optional<String> getByline() {
+    return Optional.ofNullable(this.nytdoc.getByline());
   }
 
-  public String getDateline() {
-    return this.nytdoc.getDateline();
+  public Optional<String> getDateline() {
+    return Optional.ofNullable(this.nytdoc.getDateline());
   }
 
-  public String getArticleAbstract() {
-    return this.nytdoc.getArticleAbstract();
+  public Optional<String> getArticleAbstract() {
+    return Optional.ofNullable(this.nytdoc.getArticleAbstract());
   }
 
   public String getLeadParagraph() {
     return this.nytdoc.getLeadParagraph();
   }
 
-  public String getOnlineLeadParagraph() {
-    return this.nytdoc.getOnlineLeadParagraph();
+  public Optional<String> getOnlineLeadParagraph() {
+    return Optional.ofNullable(this.nytdoc.getOnlineLeadParagraph());
   }
 
-  public String getCorrectionText() {
-    return this.nytdoc.getCorrectionText();
+  public Optional<String> getCorrectionText() {
+    return Optional.ofNullable(this.nytdoc.getCorrectionText());
   }
 
-  public String getKicker() {
-    return this.nytdoc.getKicker();
+  public Optional<String> getKicker() {
+    return Optional.ofNullable(this.nytdoc.getKicker());
   }
 
   public Optional<URL> getAlternateURL() {
@@ -119,20 +119,20 @@ public class AnnotatedNYTDocument {
     return nullListAsEmptyList(this.nytdoc.getDescriptors());
   }
 
-  public String getAuthorBiography() {
-    return this.nytdoc.getAuthorBiography();
+  public Optional<String> getAuthorBiography() {
+    return Optional.ofNullable(this.nytdoc.getAuthorBiography());
   }
 
-  public String getBanner() {
-    return this.nytdoc.getBanner();
+  public Optional<String> getBanner() {
+    return Optional.ofNullable(this.nytdoc.getBanner());
   }
 
   public List<String> getBiographicalCategories() {
     return nullListAsEmptyList(this.nytdoc.getBiographicalCategories());
   }
 
-  public String getColumnName() {
-    return this.nytdoc.getColumnName();
+  public Optional<String> getColumnName() {
+    return Optional.ofNullable(this.nytdoc.getColumnName());
   }
 
   /**
@@ -266,8 +266,8 @@ public class AnnotatedNYTDocument {
    *
    * @return the onlineSection
    */
-  public String getOnlineSection() {
-    return this.nytdoc.getOnlineSection();
+  public Optional<String> getOnlineSection() {
+    return Optional.ofNullable(this.nytdoc.getOnlineSection());
   }
 
   /**
@@ -356,8 +356,8 @@ public class AnnotatedNYTDocument {
    *
    * @return the seriesName
    */
-  public String getSeriesName() {
-    return this.nytdoc.getSeriesName();
+  public Optional<String> getSeriesName() {
+    return Optional.ofNullable(this.nytdoc.getSeriesName());
   }
 
   /**
@@ -365,8 +365,8 @@ public class AnnotatedNYTDocument {
    *
    * @return the slug
    */
-  public String getSlug() {
-    return this.nytdoc.getSlug();
+  public Optional<String> getSlug() {
+    return Optional.ofNullable(this.nytdoc.getSlug());
   }
 
   /**
@@ -423,119 +423,5 @@ public class AnnotatedNYTDocument {
    */
   public Integer getWordCount() {
     return this.nytdoc.getWordCount();
-  }
-
-  /* (non-Javadoc)
-   * @see java.lang.Object#toString()
-   */
-  @Override
-  public String toString() {
-    StringBuilder builder = new StringBuilder();
-    builder.append("AnnotatedNYTDocument [getGuid()=");
-    builder.append(getGuid());
-    builder.append(", getOnlineSectionAsList()=");
-    builder.append(getOnlineSectionAsList());
-    builder.append(", getLeadParagraphAsList()=");
-    builder.append(getLeadParagraphAsList());
-    builder.append(", getOnlineLeadParagraphAsList()=");
-    builder.append(getOnlineLeadParagraphAsList());
-    builder.append(", getBodyAsList()=");
-    builder.append(getBodyAsList());
-    builder.append(", getHeadline()=");
-    builder.append(getHeadline());
-    builder.append(", getOnlineHeadline()=");
-    builder.append(getOnlineHeadline());
-    builder.append(", getByline()=");
-    builder.append(getByline());
-    builder.append(", getDateline()=");
-    builder.append(getDateline());
-    builder.append(", getArticleAbstract()=");
-    builder.append(getArticleAbstract());
-    builder.append(", getLeadParagraph()=");
-    builder.append(getLeadParagraph());
-    builder.append(", getOnlineLeadParagraph()=");
-    builder.append(getOnlineLeadParagraph());
-    builder.append(", getCorrectionText()=");
-    builder.append(getCorrectionText());
-    builder.append(", getKicker()=");
-    builder.append(getKicker());
-    builder.append(", getAlternateURL()=");
-    builder.append(getAlternateURL());
-    builder.append(", getDescriptors()=");
-    builder.append(getDescriptors());
-    builder.append(", getAuthorBiography()=");
-    builder.append(getAuthorBiography());
-    builder.append(", getBanner()=");
-    builder.append(getBanner());
-    builder.append(", getBiographicalCategories()=");
-    builder.append(getBiographicalCategories());
-    builder.append(", getColumnName()=");
-    builder.append(getColumnName());
-    builder.append(", getColumnNumber()=");
-    builder.append(getColumnNumber());
-    builder.append(", getCorrectionDate()=");
-    builder.append(getCorrectionDate());
-    builder.append(", getCredit()=");
-    builder.append(getCredit());
-    builder.append(", getDayOfWeek()=");
-    builder.append(getDayOfWeek());
-    builder.append(", getFeaturePage()=");
-    builder.append(getFeaturePage());
-    builder.append(", getGeneralOnlineDescriptors()=");
-    builder.append(getGeneralOnlineDescriptors());
-    builder.append(", getLocations()=");
-    builder.append(getLocations());
-    builder.append(", getNames()=");
-    builder.append(getNames());
-    builder.append(", getNewsDesk()=");
-    builder.append(getNewsDesk());
-    builder.append(", getNormalizedByline()=");
-    builder.append(getNormalizedByline());
-    builder.append(", getOnlineDescriptors()=");
-    builder.append(getOnlineDescriptors());
-    builder.append(", getOnlineLocations()=");
-    builder.append(getOnlineLocations());
-    builder.append(", getOnlineOrganizations()=");
-    builder.append(getOnlineOrganizations());
-    builder.append(", getOnlinePeople()=");
-    builder.append(getOnlinePeople());
-    builder.append(", getOnlineSection()=");
-    builder.append(getOnlineSection());
-    builder.append(", getOnlineTitles()=");
-    builder.append(getOnlineTitles());
-    builder.append(", getOrganizations()=");
-    builder.append(getOrganizations());
-    builder.append(", getPage()=");
-    builder.append(getPage());
-    builder.append(", getPeople()=");
-    builder.append(getPeople());
-    builder.append(", getPublicationDate()=");
-    builder.append(getPublicationDate());
-    builder.append(", getPublicationDayOfMonth()=");
-    builder.append(getPublicationDayOfMonth());
-    builder.append(", getPublicationMonth()=");
-    builder.append(getPublicationMonth());
-    builder.append(", getPublicationYear()=");
-    builder.append(getPublicationYear());
-    builder.append(", getSection()=");
-    builder.append(getSection());
-    builder.append(", getSeriesName()=");
-    builder.append(getSeriesName());
-    builder.append(", getSlug()=");
-    builder.append(getSlug());
-    builder.append(", getSourcePath()=");
-    builder.append(getSourcePath());
-    builder.append(", getTaxonomicClassifiers()=");
-    builder.append(getTaxonomicClassifiers());
-    builder.append(", getTitles()=");
-    builder.append(getTitles());
-    builder.append(", getTypesOfMaterial()=");
-    builder.append(getTypesOfMaterial());
-    builder.append(", getUrl()=");
-    builder.append(getUrl());
-    builder.append(", getWordCount()=");
-    builder.append(getWordCount());
-    builder.append("]");
-    return builder.toString();
   }
 }
